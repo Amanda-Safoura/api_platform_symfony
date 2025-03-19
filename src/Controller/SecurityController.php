fa89eb739d4c0d5e0071a9356ba87e18579c3a33
Amanda-Safoura <amandasafoura56@gmail.com>
2025-03-19 10:58:56 +0100

    authentication added with jwt

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Entity\User;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function login(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (!$user) {
+            return $this->json(['message' => 'Invalid credentials'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'user' => $user->getEmail(),
+            'token' => $this->getUser()?->getUserIdentifier(),
+        ]);
+    }
+}
--- src/Entity/Agent.php
+++ src/Entity/Agent.php
@@ -2,10 +2,20 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Controller\CreateAgentController;
 use App\Repository\AgentRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AgentRepository::class)]
+#[Get()]
+#[Post()]
+#[GetCollection()]
+#[Delete()]
 class Agent
 {
     #[ORM\Id]
